8186e8a1031e0c8379ada7bf9f748d2cf6f77b8b
NVA92 <nazarov.victor92@yandex.ru>
2017-04-13 13:14:15 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+﻿<h1> О себе</h1>
+<?php
+$name ='Victor';
+$age = '24';
+$email = 'Nazarov.victor92@yandex.ru';
+$about = 'engeneer' ;
+echo "<br>" .'Имя:',' ',$name,PHP_EOL . "</br>";
+echo 'Возраст:'   , ' ',  $age,PHP_EOL ;
+echo "<br>". 'Адрес электронноый почты:', $email ,PHP_EOL ."</br>";
+echo 'О себе: ', ' ', $about;
+
+
+?>
